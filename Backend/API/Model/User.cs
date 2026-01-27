3a808e2a12c8043e5aab2f76a422d789f03bab0c
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2026-01-27 09:40:49 +0100

    Update User.cs

--- Backend/API/Model/User.cs
+++ Backend/API/Model/User.cs
@@ -4,11 +4,25 @@ public class User : Common
 {
     public required string Name { get; set; }
     public required string Email { get; set; }
+    public string Password { get; set; }
     
     // Foreign key - Holder role id for User
     public int RoleId { get; set; }
     // Navigation property - Holder Role objekt for User
-    public Role? Role { get; set; }
+    public Role Role { get; set; } = null!; 
 
     public List<DeliveryRoute> Routes { get; set; } = new();
+}
+
+public class RegisterUserDto
+{
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    
+}
+
+public class LoginUserDto
+{
+    public required string Email { get; set; }
+    public required string Password { get; set; }
 }
