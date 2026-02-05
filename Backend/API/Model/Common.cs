46dd4cdacf6ef6da2165491eabb8a591df3afd67
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2026-02-05 11:51:31 +0100

    Update Common.cs

--- Backend/API/Model/Common.cs
+++ Backend/API/Model/Common.cs
@@ -6,6 +6,6 @@ public class Common
 {
     [Key]
     public int Id { get; set; } 
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
